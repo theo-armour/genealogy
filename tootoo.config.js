f0bebc8fbf168854facc9ad8b9097f5473b35f90
Theo Armour <t.armour@gmail.com>
2026-07-05 16:53:33 -0700

    Add favicon and update configuration with subtitle and hidden files

--- tootoo.config.js
+++ tootoo.config.js
@@ -5,7 +5,9 @@ window.TOOTOO_CONFIG = {
   storagePrefix: 'ta-genealogy',
   sourceRepoUrl: 'https://github.com/theo-armour/genealogy',
   themeColor: '#27ae60',
-  subtitle: '',
+  subtitle: 'an open-source personal family history',
   faviconLetters: 'GN',
   faviconColor: '#27ae60',
+  hiddenFiles: [ 'tootoo.config.js' ],
+  hiddenFolders: [],
 };
